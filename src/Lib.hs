2fdb34f0e276c9b43ae1a1cacbc91d9a91c71226
Jonatan Borkowski <jonatan.borkowski@pm.me>
2020-05-01 03:25:24 +0200

    Improvement - clean up second async if the first one fails

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,16 +1,16 @@
 module Lib where
 
-import           AsyncAPI
-import qualified Data.ByteString      as B
+import           AsyncAPI             (wait, withAsync)
+import qualified Data.ByteString      as BS
 import qualified Data.ByteString.Lazy as BL
 import           Network.HTTP.Conduit
 
-getURL :: String -> IO B.ByteString
+getURL :: String -> IO BS.ByteString
 getURL url = BL.toStrict <$> simpleHttp url
 
-fetchWiki = do
-  a1 <- async (getURL "http://www.wikipedia.org/wiki/Shovel")
-  a2 <- async (getURL "http://www.wikipedia.org/wiki/Spade")
+fetchWiki =
+  withAsync (getURL "http://www.wikipedia.org/wiki/Shovel") $ \a1 ->
+  withAsync (getURL "http://www.wikipedia.org/wiki/Spade") $ \a2 -> do
   r1 <- wait a1
   r2 <- wait a2
-  print (B.length r1, B.length r2)
+  print (BS.length r1, BS.length r2)
