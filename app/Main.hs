148a10c6cd523cd1fe1b259eab0625b3b062c51f
Jonatan Borkowski <jonatan.borkowski@pm.me>
2020-05-02 02:07:54 +0200

    Add simple http server

--- app/Main.hs
+++ app/Main.hs
@@ -2,8 +2,10 @@ module Main where
 
 import qualified Data.ByteString as BS
 import           Lib
+import           Server
 
 main :: IO ()
 main = do
+  withServer
   xs <- fetchList sites
   print (map BS.length xs)
